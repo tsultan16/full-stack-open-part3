7665bfd6f7f30a01bd22ead990014584663801cb
tsultan16 <tanzids@student.unimelb.edu.au>
2025-02-13 00:00:53 +1100

    created mongoDB database model

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const Person = require('./models/person');
 
 const generateId = () => {
     return String(Math.floor(Math.random() * 1000000000000));
@@ -35,6 +14,7 @@ const checkNameExists = (name) => {
     }
     return false;
 } 
+
 
 
 const app = express();
@@ -72,7 +52,15 @@ app.get("/api", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons)
+    
+    Person.find({}).then(persons => {
+        console.log("retrieved phonebook from database:")
+        persons.forEach(p => {
+            console.log(`${p.name} ${p.number}`);
+        });
+        // mongoose.connection.close();
+        response.json(persons)
+    });
 });
 
 app.post('/api/persons', (request, response) => {
