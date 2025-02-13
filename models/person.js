e2b86416873e5281bc6e1a970bb580012f2f391d
tsultan16 <tanzids@student.unimelb.edu.au>
2025-02-13 12:57:06 +1100

    completed ex 3.19 - 3.21

--- models/person.js
+++ models/person.js
@@ -11,9 +11,40 @@ mongoose.connect(url).then(result => {
     console.log("error connecting to MongoDB: ", error.message);
 }); 
 
+// custom validator for phone number
+const phoneValidator = (val) => {
+    const parts = val.split("-");
+    // check for no hyphen or more than 1 hyphen
+    if (parts.length !== 2) {
+        return false; 
+    }
+    // pattern to check if both parts contains only numeric chars
+    const regex = /^\d+$/;
+    if (!regex.test(parts[0]) || !regex.test(parts[1])) {
+        return false;
+    }
+    // check first part contains 2 or 3 numbers
+    if(parts[0].length < 2 || parts[0].length > 3) {
+        return false;
+    }
+    return true;
+}
+
 const personSchema = new mongoose.Schema({
-    name: { type: String, minLength: 3, required: true },
-    number: { type: String, required: true },
+    name: { 
+        type: String, 
+        minLength: 3, 
+        required: [true, "name required"] 
+    },
+    number: { 
+        type: String,  
+        minLength: 8, 
+        validate: {
+            validator: phoneValidator,
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, "number required"] 
+    },
 })
 
 personSchema.set('toJSON', {
